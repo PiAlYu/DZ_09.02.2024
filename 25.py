1fa61afe74a075ae4d04c9479be69ddaaeac0774
PiAlYu <157111194+PiAlYu@users.noreply.github.com>
2024-02-06 13:30:49 +0300

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,9 +1,8 @@
-#пока не работает
 from fnmatch import fnmatch
 
 for i in range(130000, 10 ** 9, 13):
     if fnmatch(str(i), '24*68?35'):
         if int(str(i)[-3]) % 3 == 0 and int(str(i)[-3]) % 2 == 1:
-            s = str(i)[3:-3]
+            s = str(i)[2:-3]
             if all(int(j) % 2 == 0 for j in s) or len(s) == 0:
                 print(i, i // 13)
