c3bdfa7c9ce79a50df0123ce71cfa200392f359d
PiAlYu <157111194+PiAlYu@users.noreply.github.com>
2024-02-06 09:14:56 +0300

    Create 25.py

---
+++ 25.py
@@ -0,0 +1,9 @@
+#пока не работает
+from fnmatch import fnmatch
+
+for i in range(130000, 10 ** 9, 13):
+    if fnmatch(str(i), '24*68?35'):
+        if int(str(i)[-3]) % 3 == 0 and int(str(i)[-3]) % 2 == 1:
+            s = str(i)[3:-3]
+            if all(int(j) % 2 == 0 for j in s) or len(s) == 0:
+                print(i, i // 13)
